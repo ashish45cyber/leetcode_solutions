8b5d94bf4f7fee65772b2c1d73fcbc5d02856ea3
ashish45cyber <ashishbailwal939@gmail.com>
2026-07-15 21:40:15 +0530

    Time: 0 ms (100%), Space: 9.1 MB (95.16%) - LeetHub

---
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+        vector<int> lastIndex(26);
+
+        for (int i = 0; i < s.size(); i++) {
+            lastIndex[s[i] - 'a'] = i;
+        }
+
+        vector<bool> visited(26, false);
+        string ans = "";
+
+        for (int i = 0; i < s.size(); i++) {
+            char ch = s[i];
+
+            if (visited[ch - 'a'])
+                continue;
+
+            while (!ans.empty() &&
+                   ans.back() > ch &&
+                   lastIndex[ans.back() - 'a'] > i) {
+
+                visited[ans.back() - 'a'] = false;
+                ans.pop_back();
+            }
+
+            ans.push_back(ch);
+            visited[ch - 'a'] = true;
+        }
+
+        return ans;
+    }
+};
